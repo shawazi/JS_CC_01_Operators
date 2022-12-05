603ac015be26367382ac05ead2d6be5f8f846a14
shawazi <shawazisonfire@gmail.com>
2022-12-05 09:48:26 -0500

    not sure if they work

---
+++ area.js
@@ -0,0 +1,4 @@
+function area(a, b, c) {
+    s = (a + b + c)/2
+    return sqrt(s(s-a)(s-b)(s-c));
+}
---
+++ convert.js
@@ -0,0 +1,7 @@
+function convert(f){
+    return (f-32)*(5/9);
+}
+
+function celsiusToFaren(c){
+    return (c * 1.8) + 32;
+}
